d911c9dc61deccef723a9c9d0a5c240ccbd892ee
liujames <f64951576@gmail.com>
2015-03-01 05:20:08 +0800

    init

---
+++ hogsvm.0.0/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lopencv_objdetect.3.0.0 -lopencv_highgui.3.0 -lopencv_features2d.3.0.0 -lopencv_imgcodecs.3.0.0 -lopencv_videoio.3.0.0 -lopencv_core.3.0.0 -lopencv_imgproc.3.0.0 -lopencv_video.3.0.0
+
